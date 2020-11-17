3370842ba108358c1f07f56ce4672590322f4fff
Dmitry Ostanin <dmitry.ostanin@mail.ru>
2020-11-17 19:00:15 +0300

    lesson 5 task 2 - done

---
+++ homeworks/lesson_5/task_2.py
@@ -0,0 +1,6 @@
+with open("task_2.txt", "r") as file:
+    count = 0
+    for line in file:
+        count += 1
+        print(f'Line #{count}: {len(line)} symbols')
+    print(f'Total lines: {count}')
