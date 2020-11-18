b66f11afe6673101da01aa507e3bdec733d45d3c
Dmitry Ostanin <dmitry.ostanin@mail.ru>
2020-11-18 17:38:36 +0300

    lesson 5 task 3 - done

--- homeworks/lesson_5/task_1.py
+++ homeworks/lesson_5/task_1.py
@@ -1,6 +1,8 @@
-with open("task_1.txt", "w") as file:
-    line = ' '
-    while line:
-        line = input('Enter line to write to file: ')
-        file.write(f'{line}\n')
-
+try:
+    with open("task_1.txt", "w") as file:
+        line = ' '
+        while line:
+            line = input('Enter line to write to file: ')
+            file.write(f'{line}\n')
+except IOError as ioe:
+    print(f'I/O error: {ioe}')
